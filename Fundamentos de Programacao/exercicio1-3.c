8ade46e54a60137221fda9768cc5d2d4d10b92b8
maximdudai <maxim.dudai01@gmail.com>
2023-10-04 21:54:46 +0100

    first commit

---
+++ Fundamentos de Programacao/exercicio1-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Exercicio 1.3
+
+int main ()
+{		
+	printf("Please insert a number: ");
+
+	int number;
+
+	scanf("%d", &number);
+	printf("The number is %d\n", number);
+
+	return 0;
+}
---
+++ Fundamentos de Programacao/exercicio1-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Exercicio 1.4
+
+int main ()
+{		
+	printf("Please insert a number: ");
+
+	int number;
+
+	scanf("%d", &number);
+
+  int result = number * 2;
+
+	printf("The number is %d\n", result);
+
+	return 0;
+}
