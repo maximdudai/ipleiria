39fc0733076ac083d71b4032b4f8df42bc8d8805
maximdudai <maxim.dudai01@gmail.com>
2023-10-11 23:21:26 +0100

    exercicio2

--- Fundamentos de Programacao/EstruturasDecisãoSeleção/exercicio2.c
+++ Fundamentos de Programacao/EstruturasDecisãoSeleção/exercicio2.c
@@ -3,23 +3,28 @@
 
 int main() {
 
-  printf("Insira o comprimento da primeira linha");
+  printf("Insira o comprimento da primeira linha 1: ");
   int linha1;
   scanf("%d", &linha1);
-  printf("Insira o comprimento da segunda linha");
+  printf("Insira o comprimento da segunda linha 2: ");
   int linha2;
   scanf("%d", &linha2);
-  printf("Insira o comprimento da terceira linha");
+  printf("Insira o comprimento da terceira linha 3: ");
   int linha3;
   scanf("%d", &linha3);
 
-  if(linha1 == linha2 && linha2 == linha3)
-    printf("O triangulo e equilatero\n");
-  if(linha1 == linha2 || linha2 == linha3 || linha1 == linha3)
-    printf("O triangulo e escaleno\n");
-  else
-    printf("O triangulo e isosceles\n");
+  if(abs(linha1-linha2) < linha3 && linha3 < (linha1+linha2)) {
+    if(linha1 == linha2 && linha1 == linha3) 
+      printf("\nTriangulo Equilatero\n");
+    if(linha1 != linha2 && linha1!= linha3 && linha2!= linha3)
+      printf("\nTriangulo Escaleno\n");
+    else
+      printf("\nTriangulo Isosceles\n");
+  }
+  else 
+    printf("Nao e possivel formar um triangulo");
 
+  fflush(stdin);
   getchar();
   return 0;
 }
