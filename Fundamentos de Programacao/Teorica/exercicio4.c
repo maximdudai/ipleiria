456a504e9cfdb323047f6512791ba3900ed9c3ba
maximdudai <maxim.dudai01@gmail.com>
2023-10-11 20:52:43 +0100

    exercicio 5

--- Fundamentos de Programacao/Teorica/exercicio4.c
+++ Fundamentos de Programacao/Teorica/exercicio4.c
@@ -14,28 +14,31 @@ int main() {
 
     printf("Please enter the operation: ");
     char operation;
-    scanf("%c", &operation);
+    scanf(" %c", &operation);
 
     int somNumber, subNumber, multNumber, divNumber;
 
     switch (operation)
     {
       case '+':
-        int somNumber = first + second;
+        somNumber = first + second;
         printf("\nThe sum of the two numbers is %d", somNumber);
         break;
       case '-':
-        int subNumber = first - second;
+        subNumber = first - second;
         printf("\nThe substraction of the two number is %d", subNumber);
         break;
 
       case '*':
-        int multNumber = first * second;
+        if(first == 0 || second == 0)
+          return printf("Erro – Divisão por zero");
+          
+        multNumber = first * second;
         printf("\nThe multiplication of the two number is %d", multNumber);
         break;
 
       case '/':
-        int divNumber = first / second;
+        divNumber = first / second;
         printf("\nThe division of the two number is %d", divNumber);
         break;
       default:
